0e96603a4f7dd326306c90b61f16a939a8b3df1f
Louis Afful <louisafful1@gmail.com>
2024-09-12 08:50:15 +0000

    Add notFound route error and enhance errorHandler to handle CastError for invalid ObjectId

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,12 +1,25 @@
-const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-    res.status(statusCode)
+// Routes that do not exist
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+}
 
-    res.json({
+// error in routes
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    // Handling CastError for invalid ObjectId
+    if (err.name === "CastError" && err.kind ==="ObjectId") {
+        statusCode = 404
+        err.message = "Resource not found"
+    }
+
+    res.status(statusCode).json({
         message: err.message || "An unknown error occurred",
         stack: process.env.NODE_ENV === "development" ? err.stack : null
     })
 
 }
 
-export default errorHandler
+export {notFound, errorHandler} 
